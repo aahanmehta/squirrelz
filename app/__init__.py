3d922abd773cae5e7186beabefb08133989aebdb
Weichen <wliu30@stuy.edu>
2023-05-04 13:43:19 -0400

    a

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,11 @@
 from flask import Flask, render_template, request, session, redirect, url_for, make_response, db_tools, random, requests
+
+app = Flask(__name__)    #create Flask object
+
+@app.route('/')
+def homepage():
+    return render_template('home.html')
+
+if __name__ == "__main__": #false if this file imported as module
+    app.debug = True 
+    app.run()
