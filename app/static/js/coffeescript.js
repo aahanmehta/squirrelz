5cd6a3da8ad81bee6a2b20c7a3ee37898fad6f8d
Jeff Chen <cytastorm@gmail.com>
2023-05-05 14:03:00 -0400

    setup for js

---
+++ app/static/js/coffeescript.js
@@ -0,0 +1,15 @@
+console.log("I'm here")
+
+var button = document.getElementById("button")
+
+var checkBox = function(whichBox) {
+    // Get the checkbox
+    var checkBox = document.getElementById(whichBox);
+    // Get the output text
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+    //   text.style.display = "block";
+    } else {
+    //   text.style.display = "none";
+    }
+  } 
