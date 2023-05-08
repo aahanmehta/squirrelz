405d859eb42ab0a1adb6e7b5705fe07087e1ddd4
Jeff Chen <cytastorm@gmail.com>
2023-05-07 22:25:38 -0400

    linked js

--- app/static/js/state.js
+++ app/static/js/state.js
@@ -3,13 +3,11 @@ console.log("I'm here")
 var button = document.getElementById("button")
 
 var checkBox = function(whichBox) {
-    // Get the checkbox
     var checkBox = document.getElementById(whichBox);
-    // Get the output text
-    // If the checkbox is checked, display the output text
+    var text = document.getElementById("checktext");
     if (checkBox.checked == true){
-    //   text.style.display = "block";
+      text.style.display = "block";
     } else {
-    //   text.style.display = "none";
+      text.style.display = "none";
     }
-  } 
+} 
