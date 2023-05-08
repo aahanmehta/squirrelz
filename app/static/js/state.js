d1a9a00e20b5f81156724c06186ecd39cd63a2a2
Jeff Chen <cytastorm@gmail.com>
2023-05-07 22:37:54 -0400

    basic js functionality

--- app/static/js/state.js
+++ app/static/js/state.js
@@ -2,6 +2,14 @@ console.log("I'm here")
 
 var button = document.getElementById("button")
 
+var buttonFunc = function() {
+  var text = document.getElementById("btntext");
+  if (text.style.display == "block"){
+    text.style.display = "none";
+  } else {
+    text.style.display = "block";
+  }
+}
 var checkBox = function(whichBox) {
     var checkBox = document.getElementById(whichBox);
     var text = document.getElementById("checktext");
