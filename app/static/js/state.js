188238a4f2cf0f08de48a354cf8ea477f2ffad5f
Jeff <cytastorm@gmail.com>
2023-05-23 00:28:12 -0400

    i hate hate hate hate hate

--- app/static/js/state.js
+++ app/static/js/state.js
@@ -1,5 +1,29 @@
 import { statesData } from "./statemap.js";
+import { abbrState } from "./stateNameChanger.js";
 
+//XMLHttpRequest for getting specific state data
+var getStateData = function(state) {
+  //console.log(selectElem.options[selectElem.selectedIndex].id);
+  //var formElem = selectElem.options[selectElem.selectedIndex].id;
+  let param = state;
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    //this function is called when a response is received from the form's endpoint
+    if (this.readyState === this.DONE) {
+      //this code block executes when the response data is finished loading
+      var responseText = xhttp.responseText; //handle this variable however you like
+      //if you want to treat as JSON data, you can use this line
+      responseText = (JSON.parse(responseText));
+      console.log(responseText);
+      state_ufo(responseText);
+    }
+  };
+  //opens a request to send the data to the URL form.action via form.method
+  //note the false at the end of the xhttp.open call
+  xhttp.open("GET", "https://ergo.newjeans.live:4999/get_state_data?state="+state, true);
+  xhttp.send();
+  return false;
+}
 //leaflet map
 var map = L.map('map').setView([38.09302572405113, -96.76474497954052], 5);
 
@@ -44,6 +68,9 @@ function showSpecificGraph(e) {
     //currently just zooms
     //map.fitBounds(e.target.getBounds());
 
+    var name = abbrState(e.target.feature.properties.name, 'abbr');
+    //console.log(name);
+    getStateData(name);
     //add show graph functionality
 }
 
@@ -78,13 +105,13 @@ var svg = d3.select("#my_dataviz")
 
 // Parse the Data
 d3.json("https://ergo.newjeans.live:4999/scatter-car-drunk", function(data) {
-  console.log(Object.values(data));
+  //console.log(Object.values(data));
   var data = Object.values(data);
 
 
   // Add X axis
   var x = d3.scaleLinear()
-    .domain([1, 5])
+    .domain([0, 5])
     .range([ 0, width ]);
   svg.append("g")
     .attr("transform", "translate(0," + height + ")")
@@ -161,13 +188,12 @@ d3.json("https://ergo.newjeans.live:4999/scatter-car-drunk", function(data) {
 
  })
 
-
-//ethanol consumption to ufo sightings graph
+//ethanol consumption to ufo sightings graph per year
 var margin = {top: 10, right: 30, bottom: 60, left: 60},
     width = 1280 - margin.left - margin.right,
     height = 600 - margin.top - margin.bottom;
 // append the svg object to the body of the page
-var svg2 = d3.select("#state_specific_graph")
+var svg2 = d3.select("#my_dataviz2")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -178,39 +204,15 @@ var svg2 = d3.select("#state_specific_graph")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
-var getStateData = function(state) {
-  //console.log(selectElem.options[selectElem.selectedIndex].id);
-  //var formElem = selectElem.options[selectElem.selectedIndex].id;
-  let param = state;
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    //this function is called when a response is received from the form's endpoint
-    if (this.readyState === this.DONE) {
-      //this code block executes when the response data is finished loading
-      var responseText = xhttp.responseText; //handle this variable however you like
-      //if you want to treat as JSON data, you can use this line
-      //var json = JSON.parse(xhttp.responseText)
-      console.log("Response received: " + responseText);
-    }
-  };
-  //opens a request to send the data to the URL form.action via form.method
-  //note the false at the end of the xhttp.open call
-  xhttp.open("GET", "https://ergo.newjeans.live:4999/get_state_data?state=${state}", true);
-  xhttp.send();
-  return false;
-}
-
-console.log("testing line 202");
-getStateData("ny");
 // Parse the Data
 d3.json("https://ergo.newjeans.live:4999/scatter-ufo-drunk", function(data) {
-  console.log(Object.values(data));
+  //console.log(Object.values(data));
   var data = Object.values(data);
 
-
+  console.log(data);
   // Add X axis
   var x = d3.scaleLinear()
-    .domain([1, 5])
+    .domain([0, 5])
     .range([ 0, width ]);
   svg2.append("g")
     .attr("transform", "translate(0," + height + ")")
@@ -232,6 +234,110 @@ d3.json("https://ergo.newjeans.live:4999/scatter-ufo-drunk", function(data) {
     .call(d3.axisLeft(y));
 
   svg2.append("text")
+  .attr("class", "y label")
+  .attr("transform", "rotate(-90),translate(-"+height/2+", -50)")
+  .attr("text-anchor", "middle")
+  .attr("dy", ".75em")
+  .text("Number of UFO Sightings");
+  
+  var tooltip = d3.select("#my_dataviz2")
+  .append("div")
+  .style("opacity", 0)
+  .attr("class", "tooltip")
+  .style("background-color", "white")
+  .style("border", "solid")
+  .style("border-width", "1px")
+  .style("border-radius", "5px")
+  .style("padding", "10px")
+  .style("position", "absolute")
+  // A function that change this tooltip when the user hover a point.
+  // Its opacity is set to 1: we can now see it. Plus it set the text and position of tooltip depending on the datapoint (d)
+  var mouseover = function(d) {
+    tooltip
+      .style("opacity", 1)
+  }
+
+  var mousemove = function(d) {
+   tooltip
+     .html(d.STATE + ": " + d.ethanol_per_capita + " gallons, " + d.ufo_sighting + "UFO sightings" )
+     .style("left", d3.event.pageX + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
+     .style("top", d3.event.pageY + "px")
+  }
+
+  // A function that change this tooltip when the leaves a point: just need to set opacity to 0 again
+  var mouseleave = function(d) {
+    tooltip
+      .transition()
+      .duration(200)
+      .style("opacity", 0)
+  }
+  // Add dots
+  svg2.append('g')
+    .selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+      .attr("cx", function (d) { return x(d.ethanol_per_capita); } )
+      .attr("cy", function (d) { return y(d.ufo_sightings); } )
+      .attr("r", 10)
+      .style("fill", "#69b3a2")
+      .style("opacity", 0.3)
+      .style("stroke", "white")
+    .on("mouseover", mouseover )
+    .on("mousemove", mousemove )
+    .on("mouseleave", mouseleave )
+
+ })
+//ethanol consumption to ufo sightings graph state specific
+var margin = {top: 10, right: 30, bottom: 60, left: 60},
+    width = 1280 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
+// append the svg object to the body of the page
+var svg3 = d3.select("#state_specific_graph")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .call(d3.zoom().on("zoom", function () {
+       svg3.attr("transform", d3.event.transform)
+  }))
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+
+
+console.log("testing line 202");
+getStateData("ny");
+// Parse the Data
+function state_ufo(data) {
+  var data = Object.values(data);
+  console.log(data);
+
+
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain([0,5])
+    .range([ 0, width ]);
+  svg3.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+  svg3.append("text")
+  .attr("class", "x label")
+  .attr("text-anchor", "middle")
+  .attr("transform", "translate(-"+width/2+",0)")
+  .attr("x", width)
+  .attr("y", height + 40)
+  .text("Ethanol Consumed");
+
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([1, 500])
+    .range([ height, 5]);
+  svg3.append("g")
+    .call(d3.axisLeft(y));
+
+  svg3.append("text")
   .attr("class", "y label")
   .attr("transform", "rotate(-90),translate(-"+height/2+", -50)")
   .attr("text-anchor", "middle")
@@ -257,7 +363,7 @@ d3.json("https://ergo.newjeans.live:4999/scatter-ufo-drunk", function(data) {
 
   var mousemove = function(d) {
    tooltip
-     .html(d.STATE + ": " + d.ethanol_per_capita + " gallons, " + d.ufo_sighting + "UFO sightings" )
+     .html(d.Year + ": " + d.sightings + " UFO sightings " + d.ethanol_per_capita + " gallons of ethanol consumed per capita")
      .style("left", d3.event.pageX + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
      .style("top", d3.event.pageY + "px")
   }
@@ -269,14 +375,17 @@ d3.json("https://ergo.newjeans.live:4999/scatter-ufo-drunk", function(data) {
       .duration(200)
       .style("opacity", 0)
   }
+  // Remove dots
+
+  svg3.selectAll("circle").remove();
   // Add dots
-  svg2.append('g')
+  svg3.append('g')
     .selectAll("dot")
     .data(data)
     .enter()
     .append("circle")
       .attr("cx", function (d) { return x(d.ethanol_per_capita); } )
-      .attr("cy", function (d) { return y(d.ufo_sightings); } )
+      .attr("cy", function (d) { return y(d.sightings); } )
       .attr("r", 10)
       .style("fill", "#69b3a2")
       .style("opacity", 0.3)
@@ -285,108 +394,7 @@ d3.json("https://ergo.newjeans.live:4999/scatter-ufo-drunk", function(data) {
     .on("mousemove", mousemove )
     .on("mouseleave", mouseleave )
 
- })
-
-//ethanol consumption to ufo sightings graph per year (state specific)
-var margin = {top: 10, right: 30, bottom: 60, left: 60},
-    width = 1280 - margin.left - margin.right,
-    height = 600 - margin.top - margin.bottom;
-// append the svg object to the body of the page
-var svg2 = d3.select("#state_specific_graph2")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .call(d3.zoom().on("zoom", function () {
-       svg2.attr("transform", d3.event.transform)
-  }))
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
-
-// Parse the Data
-d3.json("https://ergo.newjeans.live:4999/scatter-ufo-drunk", function(data) {
-  console.log(Object.values(data));
-  var data = Object.values(data);
-
-
-  // Add X axis
-  var x = d3.scaleLinear()
-    .domain([1, 5])
-    .range([ 0, width ]);
-  svg2.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
-
-  svg2.append("text")
-  .attr("class", "x label")
-  .attr("text-anchor", "middle")
-  .attr("transform", "translate(-"+width/2+",0)")
-  .attr("x", width)
-  .attr("y", height + 40)
-  .text("Ethanol Consumed Per Capita");
-
-  // Add Y axis
-  var y = d3.scaleLog()
-    .domain([1, 10000])
-    .range([ height, 5]);
-  svg2.append("g")
-    .call(d3.axisLeft(y));
-
-  svg2.append("text")
-  .attr("class", "y label")
-  .attr("transform", "rotate(-90),translate(-"+height/2+", -50)")
-  .attr("text-anchor", "middle")
-  .attr("dy", ".75em")
-  .text("Number of UFO Sightings");
-  
-  var tooltip = d3.select("#state_specific_graph2")
-  .append("div")
-  .style("opacity", 0)
-  .attr("class", "tooltip")
-  .style("background-color", "white")
-  .style("border", "solid")
-  .style("border-width", "1px")
-  .style("border-radius", "5px")
-  .style("padding", "10px")
-  .style("position", "absolute")
-  // A function that change this tooltip when the user hover a point.
-  // Its opacity is set to 1: we can now see it. Plus it set the text and position of tooltip depending on the datapoint (d)
-  var mouseover = function(d) {
-    tooltip
-      .style("opacity", 1)
-  }
-
-  var mousemove = function(d) {
-   tooltip
-     .html(d.STATE + ": " + d.ethanol_per_capita + " gallons, " + d.ufo_sighting + "UFO sightings" )
-     .style("left", d3.event.pageX + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
-     .style("top", d3.event.pageY + "px")
-  }
-
-  // A function that change this tooltip when the leaves a point: just need to set opacity to 0 again
-  var mouseleave = function(d) {
-    tooltip
-      .transition()
-      .duration(200)
-      .style("opacity", 0)
-  }
-  // Add dots
-  svg2.append('g')
-    .selectAll("dot")
-    .data(data)
-    .enter()
-    .append("circle")
-      .attr("cx", function (d) { return x(d.ethanol_per_capita); } )
-      .attr("cy", function (d) { return y(d.ufo_sightings); } )
-      .attr("r", 10)
-      .style("fill", "#69b3a2")
-      .style("opacity", 0.3)
-      .style("stroke", "white")
-    .on("mouseover", mouseover )
-    .on("mousemove", mousemove )
-    .on("mouseleave", mouseleave )
-
- })
+ }
 const form = document.getElementById("form");
 const selectElem = document.getElementById("sel_id");
 
